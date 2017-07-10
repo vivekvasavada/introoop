f1d037483a26df11a8a6e7548f9f89517089e2b8
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-10 14:30:44 -0400

    added a class

---
+++ Introtooop/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introtooop
+{
+    class Cat
+    {
+        //inside of our class, e create the blueprint of our object.
+        //classes are templates in oop that we use to create objects.
+        // akways be in Pascal case.
+    }
+}
--- Introtooop/Program.cs
+++ Introtooop/Program.cs
@@ -10,6 +10,18 @@ namespace Introtooop
     {
         static void Main(string[] args)
         {
+            //When we initialize a new object, it is called instantiating
+            //object has to be instantiated before it can be used
+            //all objects of a specific type are created from the same template-class
+            //all objects from a class will have the same number and types of states but
+            //each value can have it's own unique values
+
+            //format
+            //ClassName objectName= new ClassName ();
+            Cat mittens = new Cat();
+            //to create a nrw object, we use a special method called a CONSTRUCTOR
+            //ball,stiches,color,logo,sphere,smooth
+            //thrown,rolled,stays still,caught,bounced,curves
         }
     }
 }
